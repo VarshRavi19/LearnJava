bf32a0636af0b80ec34014e5eb78e6cc7f969013
VarshRavi19 <141304935+VarshRavi19@users.noreply.github.com>
2023-09-16 16:47:33 +0530

    Add files via upload

---
+++ SumOfValuesinArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    int a[]= {10,20,30,40,50};
+    int sum=0;
+    for(int b:a){
+      sum+=b;
+     }
+    System.out.println(sum);
+   }
+ }
