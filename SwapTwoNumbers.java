4dd035979e3c75fcc41e670659d9fdb695f908da
VarshRavi19 <141304935+VarshRavi19@users.noreply.github.com>
2023-09-16 16:30:53 +0530

    Add files via upload

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+   int a = 10;
+   int b=20;
+   int c;
+   System.out.println( " Before Swapping" + "\n" + a + "\n"+ b);
+   c=a;
+   a=b;
+   b=c;
+   System.out.println( "After Swapping"+ "\n" + a + "\n" + b);
+   }
+ }
